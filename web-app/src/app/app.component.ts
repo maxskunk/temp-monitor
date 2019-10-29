9b8529d57ac92a5442007ebfb5093b0c9b343bd2
Brian J Weis <brian@brianweis.net>
2019-10-28 21:24:05 -0600

    Got the charts working

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-app';
+
+  data: any;
+  options: any;
+  constructor() {
+
+  }
+  ngOnInit() {
+    this.data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    };
+    this.options = {
+      onAnimationComplete: function () {
+        this.showTooltip(this.segments, true);
+      },
+
+      tooltipEvents: [],
+      tooltips: {
+        intersect: true
+      },
+      showTooltips: true,
+      title: {
+        display: true,
+        text: 'My Title',
+        fontSize: 16
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+
+  }
 }
